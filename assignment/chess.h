6099a2d9bf0a5f4b3dab7df71fb5f1718e344bbd
BastianHaider <bastian.haider@gmx.at>
2018-10-30 06:45:57 -0700

    a few tests less

--- assignment/chess.h
+++ assignment/chess.h
@@ -1,32 +1,48 @@
 /*----------------------------------------------------------
- *				HTBLA-Leonding / Klasse: 2AHDV
+ *				HTBLA-Leonding / Klasse: 2DHIF
  * ---------------------------------------------------------
- * Exercise Number: 0
+ * Exercise Number: 6
  * Title:			chess.h
- * Author:			P. Bauer
+ * Author:			Bastian Haider
  * Due Date:		October 15, 2014
  * ----------------------------------------------------------
  * Description:
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+
+ #include <stdbool.h>
+
  enum Color { White, Black };
- enum ChessPiece{
-   Pawn, Knight, Rook, Bishop, Queen, King
+ enum PieceType{
+   Pawn, Knight, Rook, Bishop, Queen, King, NoPiece
  };
 
 struct ChessPiece{
   enum Color color;
-  enum ChessPiece piece;
+  enum PieceType piece;
 };
 
 struct ChessSquare{
-  bool is_set;
+  bool is_occupied;
   struct ChessPiece piece;
 };
 
 typedef ChessSquare ChessBoard[8][8];
+typedef ChessSquare Chessboard[8][8];
 
-bool is_piece(struct ChessBoard board, enum Color color, enum PieceType type);
+bool is_piece(struct ChessPiece piece, enum Color color, enum PieceType type);
 
-void init_chess_board(ChessBoard chess_board);  
+void init_chess_board(ChessBoard chess_board);
+
+typedef int Rank;
+typedef char File;
+struct ChessSquare* get_square(ChessBoard chess_board,File file,Rank rank);
+
+bool is_square_occupied(ChessBoard chess_board,File file,Rank rank);
+
+bool add_piece(ChessBoard chess_board,File a,Rank rank, ChessPiece black_rook);
+
+struct ChessPiece get_piece(ChessBoard chess_board, File a, Rank rank);
+
+bool setup_chess_board(File file, Rank rank);
